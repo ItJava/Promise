f941e4f2ea00f8d7c13b8906a2d5a439fb2535f9
itjavawfc@163.com <itjavawfc@163.com>
2021-04-15 20:16:08 +0800

    4_Promise封装练习-fs模块练习.js

---
+++ 1_Promise基本使用/4_Promise封装练习-fs模块练习.js
@@ -0,0 +1,22 @@
+/*
+封装一个函数 mineReadFile 读取文件内容
+参数：path  文件路径
+返回：promise  对象
+ */
+
+function mineReadFile(path){
+    return  new Promise((resolve, reject) => {
+        require('fs').readFile(path,(err,data)=>{
+            //判断 如果错误
+            if(err) reject(err);
+            //成功
+            resolve(data)
+        })
+    })
+}
+
+mineReadFile('./resource/content.txt').then(value => {
+    console.log(value.toString())
+},reason => {
+    console.log(reason)
+})
